735f204861f3710835be1ad834787dc566c4a0c4
vinegraft <982042+vinegraft@users.noreply.github.com>
2024-09-25 13:24:09 -0400

    Complete numbers exercises

--- ruby_basics/1_data_types/exercises/numbers_exercises.rb
+++ ruby_basics/1_data_types/exercises/numbers_exercises.rb
@@ -5,32 +5,40 @@ end
 
 def subtract(a, b)
   # return the result of subtracting b from a
+  a - b
 end
 
 def multiply(a, b)
   # return the result of multiplying a times b
+  a * b
 end
 
 def divide(a, b)
   # return the result of dividing a by b
+  a / b
 end
 
 def remainder(a, b)
   # return the remainder of dividing a by b using the modulo operator
+  a % b
 end
 
 def float_division(a, b)
   # return the result of dividing a by b as a float, rather than an integer
+  a / b.to_f
 end
 
 def string_to_number(string)
+  string.to_i
   # return the result of converting a string into an integer
 end
 
 def even?(number)
+  number.even?
   # return true if the number is even (hint: use integer's even? method)
 end
 
 def odd?(number)
+  number.odd?
   # return true if the number is odd (hint: use integer's odd? method)
 end
